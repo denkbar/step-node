12f71465760b552bf705d2c6d1b1852817a81bf6
Jerome Brongniart <jerome.brongniart@exense.ch>
2018-08-09 16:28:02 +0200

    Improve unit testing of the onError

--- step-node-agent/test/test.js
+++ step-node-agent/test/test.js
@@ -7,13 +7,18 @@ const assert = require('assert')
   assert.equal(output.payload.properties.Property1, 'Prop1')
 })()
 
+global.isOnErrorCalled = false
+
 ;(async () => {
   const errorMsg1 = 'Error - rethrow'
   const output1 = await runner.run('ErrorTestKW', {ErrorMsg: errorMsg1, rethrow_error: true})
   assert.equal(output1.error, errorMsg1)
+  assert.equal(global.isOnErrorCalled, true)
+
+  global.isOnErrorCalled = false
 
   const errorMsg2 = 'Error - do not rethrow'
   const output2 = await runner.run('ErrorTestKW', {ErrorMsg: errorMsg2, rethrow_error: false})
-
   assert.equal(output2.error, undefined)
+  assert.equal(global.isOnErrorCalled, true)
 })()
